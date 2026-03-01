39b4ca1ba6ad8a909251f08a6dac12ba96c82ad6
unknown <thalita.sok@student.passerellesnumeriques.org>
2026-03-01 12:31:47 +0700

    update author

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css'
+import './index.css'
 import AuthorRoute from './author/AuthorRoute/AuthorRoute'
 
 function App() {
-  return <AuthorRoute />
+  return (
+  <AuthorRoute />
+);
 }
 
 export default App
